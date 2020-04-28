6f5cf9586f327ce70f42e646657582ea5ac28edf
bertibertig <sascha.bertleff@gmail.com>
2020-04-29 01:12:16 +0200

    +Added Player Animation for Idle and Walking, + Added Skeleton for Dialogues

---
+++ Assets/Editor/DialogueAsset.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public class DialogueAsset
+{
+    [MenuItem("Assets/Create/Dialogue")]
+    public static void CreateAsset()
+    {
+        CustomAssetUtility.CreateAsset<Dialogue>();
+    }
+}
---
+++ Assets/Scripts/DialogueHandler/DialogueElement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueElement
+{
+    public enum AvatarPos { left, right};
+    public string speakerName;
+    public AvatarPos speakerPosition;
+    public Texture2D speakerPic;
+    public string dialogueText;
+    public GUIStyle dialogueFont;
+    public float playbackSpeed;
+    public AudioClip soundForTextScrolling;
+    public string nameOfMethodeToActivateBeforeNextText;
+    public bool waitUntilMethodeIsFinished;
+    public bool isMultichoice;
+    [Header("Is only used, if isMultichoice is true")]
+    public MultiChoiceDialogue multiChoiceDialogue;
+}
+
