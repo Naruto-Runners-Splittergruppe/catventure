4ffaf9d8ec175350149a4a74b60a7fca295e2bfe
bertibertig <sascha.bertleff@gmail.com>
2020-05-03 17:22:18 +0200

    Added Simple VariableEventSystem

---
+++ Assets/Scripts/Utilities/Variables Event System/Event.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event<T>
+{
+	private T eventVariable;
+	private EventHandler eventVariableSet;
+	private EventHandler eventVariableGet;
+
+	public EventHandler EventVariableGet
+	{
+		get { return eventVariableGet; }
+		set { eventVariableGet = value; }
+	}
+
+
+	public EventHandler EventVariableSet
+	{
+		get { return eventVariableSet; }
+		set { eventVariableSet = value; }
+	}
+
+
+	public T EventVariable
+	{
+		get
+		{
+			eventVariableGet.Invoke(this, null);
+			return eventVariable;
+		}
+		set
+		{
+			eventVariableSet.Invoke(this, null);
+			eventVariable = value;
+		}
+	}
+
+	public Guid Id { get; internal set; }
+	public string Name { get; internal set; }
+
+	public string Description { get; internal set; }
+
+	public static Event<S> CreateInstance<S>(string name, string description, S value)
+	{
+		Event <S> createdEvent = new Event<S>() {Id = new Guid(), Name = name, Description = description, EventVariable = value};
+		return createdEvent;
+	}
+}
---
+++ Assets/Scripts/Utilities/Variables Event System/VariableEventSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableEventSystem : MonoBehaviour
+{
+    public List<Event<bool>> boolEvents;
+    public List<Event<int>> intEvents { get; set; }
+    public List<Event<float>> floatEvents { get; set; }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
