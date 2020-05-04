4887c91dfeab57c523db4e3dfa0a3384c23a0934
LukasTiefenthaler <lukas.tiefenthaler1@gmail.com>
2020-05-04 16:03:40 +0200

    CheckPoint System

---
+++ Assets/Scripts/Checkpoint/PlayerPos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPos : MonoBehaviour
+{
+    private GameMaster gm;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        transform.position = gm.lastCheckPointPos;
+    }
+    // for testing (kills player)
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
