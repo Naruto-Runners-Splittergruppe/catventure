86f0298e9445be3e1429827f8ca068f8eab81f49
Andreas Kitzberger <kitzberger_a1@outlook.at>
2020-05-04 18:26:59 +0200

    Lifes & Standard Enemy Script

---
+++ Assets/Scripts/Enemy/StandardEnemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardEnemy : MonoBehaviour
+{
+    private Lifes lifes;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        lifes = player.GetComponent<Lifes>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D colider)
+    {
+        if (colider.tag == "Player" && lifes.Invicible == false)
+        {
+            //Gameobject player = Gameobject.FindGameobjectWithTag("Player");
+            //PolygonCollider2D pc2d = player.GetComponent<PolygonCollider2D>();
+            lifes.Invicible = true;
+            lifes.Health--;            
+            StartCoroutine("WaitSeconds", 10);           
+        }
+    } 
+    
+    private IEnumerator WaitSeconds(int i)
+    {
+        yield return new WaitForSeconds(i);
+        lifes.Invicible = false;
+    }
+}
---
+++ Assets/Scripts/Player/Lifes.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lifes : MonoBehaviour
+{
+    public GameObject heart1, heart2;
+    
+    private int health;
+    public int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+
+    public bool invicible;
+    public bool Invicible
+    {
+        get { return invicible; }
+        set { invicible = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 2;
+        invicible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health == 2)
+        {
+            heart1.gameObject.SetActive(true);
+            heart2.gameObject.SetActive(true);
+        } else if(health == 1)
+        {
+            heart1.gameObject.SetActive(true);
+            heart2.gameObject.SetActive(false);
+        } else if(health == 0)
+        {
+            heart1.gameObject.SetActive(false);
+            heart2.gameObject.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }     
+}
