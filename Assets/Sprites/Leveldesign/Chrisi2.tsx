5d74f920e5570cf5c1da8deced83033b4aa5e327
trees-are-awesome <64367012+trees-are-awesome@users.noreply.github.com>
2020-06-04 20:51:58 +0200

    Level 1 - Karte importiert
    Die Karte ist oben, aber die Scene ist noch nicht spielbar.

---
+++ Assets/Sprites/Leveldesign/Chrisi2.tsx
@@ -0,0 +1,298 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="Chrisi2" tilewidth="349" tileheight="198" tilecount="48" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="349" height="198" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Bridge.PNG"/>
+ </tile>
+ <tile id="1">
+  <image width="50" height="34" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cave_bottom.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0.727273" y="22.7273">
+    <polygon points="-0.545455,-0.363636 49.3466,-0.448864 49.3409,11.2102 -0.551136,11.2102"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <image width="50" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cave_corner_left_bottom.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.25" y="0.5">
+    <polygon points="0,-0.375 12,-0.5 11.875,37.875 49.875,37.875 50.125,49.5 -0.25,49.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <image width="50" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cave_corner_left_top.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.375" y="0.375">
+    <polygon points="0,-0.375 49.625,-0.125 49.625,11 11.75,11.5 11.5,49.625 -0.375,49.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <image width="50" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cave_corner_right_bottom.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="38" y="0">
+    <polygon points="0,0 0.25,37.875 -38.125,37.875 -38.25,50.125 12.25,50.125 12,0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <image width="50" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cave_corner_right_top.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.125" y="-0.25">
+    <polygon points="0,0 0,12 37.125,12.25 37.75,50.25 49.875,50.25 49.875,0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <image width="50" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cave_diagonal.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.125" y="49.625">
+    <polygon points="-0.125,0.625 49.875,0.25 50,-49.625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <image width="50" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cave_diagonal_2.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.125" y="0.25">
+    <polygon points="0,0 0.125,49.875 50.25,49.625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <image width="34" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cave_left.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.25" y="0.25">
+    <polygon points="0,0 -0.25,49.625 12,49.625 11.375,-0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <image width="50" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cave_left_entry.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.25" y="0.25">
+    <polygon points="0,0 -0.125,12.875 49.625,12.75 49.625,-0.375"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <image width="50" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cave_right.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="37.875" y="0.625">
+    <polygon points="0.125,-0.5 0.25,49.5 12.125,49.5 12,-0.625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <image width="50" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cave_right_entry.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.125" y="0">
+    <polygon points="0,0 -0.125,12.125 49.75,12.25 49.875,-0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <image width="50" height="34" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cave_top.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.125" y="0.875">
+    <polygon points="0,-1 50.125,-1 50,11.125 0.125,11.375"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <image width="50" height="149" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cloud_diagonal.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.625" y="57.375">
+    <polygon points="-0.375,2.25 49.125,-45.75 49.125,91.625 -0.5,91.375"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <image width="50" height="149" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cloud_diagonal_2.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="12.875">
+    <polygon points="0,0 49.75,47 50.125,135.75 0.125,135.875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <image width="50" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cloud_Floor.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.375" y="14.875">
+    <polygon points="-0.5,0 49.25,-0.75 49.375,102.875 -0.375,103"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <image width="60" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cloud_Floor_left_corner.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.875" y="14.75">
+    <polygon points="0.125,-0.25 51,-0.625 50.875,103.25 0.125,103.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <image width="60" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cloud_Floor_right_corner.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.375" y="14.75">
+    <polygon points="-0.5,-0.25 50.5,-0.875 50.875,103.25 -0.625,103.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <image width="74" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cloud_platform.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="12.5" y="14.5">
+    <polygon points="0,0 0.5,19.125 53,19.75 52.375,-0.375"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <image width="60" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cloud_platform_left.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="9" y="14.125">
+    <polygon points="0,0 50.75,-0.875 51.25,21.375 0.625,21"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <image width="60" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Cloud_platform_right.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.5" y="13.75">
+    <polygon points="-0.5,-0.125 49.75,0.375 49.875,19.5 -0.5,20.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <image width="142" height="198" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Clouds_1.PNG"/>
+ </tile>
+ <tile id="22">
+  <image width="142" height="198" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Clouds_2.PNG"/>
+ </tile>
+ <tile id="23">
+  <image width="349" height="198" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Fence.PNG"/>
+ </tile>
+ <tile id="24">
+  <image width="50" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Garn_ammo.PNG"/>
+ </tile>
+ <tile id="25">
+  <image width="50" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Ground_1.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.5" y="14.25">
+    <polygon points="0,0 49.5,-0.5 49.25,103.5 -1,103.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <image width="50" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Ground_2.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.5" y="13">
+    <polygon points="0,0 49,-0.25 49.25,104.5 -0.75,105"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <image width="50" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Ground_3.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.75" y="13.25">
+    <polygon points="0,0 48.75,-0.25 49.25,104.75 -0.5,105.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <image width="50" height="147" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Ground_diagonal.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.75" y="66.25">
+    <polygon points="-0.75,-0.25 49,-50 50,80.25 -1,80.75"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <image width="50" height="147" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Ground_diagonal_2.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="16.75">
+    <polygon points="0,0 50,50.5 50,130 0.25,130.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <image width="56" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Ground_leftside.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="13.75">
+    <polygon points="0,0 50.25,-0.75 49.75,103.75 -0.75,104"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <image width="56" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Ground_rightside.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.5" y="15.5">
+    <polygon points="0,0 48.5,-0.75 50,102.25 -0.5,102.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <image width="349" height="198" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/House_background.PNG"/>
+ </tile>
+ <tile id="33">
+  <image width="349" height="198" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/House_behind_objects.PNG"/>
+ </tile>
+ <tile id="34">
+  <image width="349" height="198" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/House_front_objects.PNG"/>
+ </tile>
+ <tile id="35">
+  <image width="349" height="198" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/House_mockup.PNG"/>
+ </tile>
+ <tile id="36">
+  <image width="349" height="198" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/House_overworld.PNG"/>
+ </tile>
+ <tile id="37">
+  <image width="50" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Katzenminze.PNG"/>
+ </tile>
+ <tile id="38">
+  <image width="349" height="198" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/LKW.PNG"/>
+ </tile>
+ <tile id="39">
+  <image width="68" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Platform.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10" y="13.25">
+    <polygon points="0,0 1.75,12.25 50.5,13 53.25,-0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <image width="56" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Platform_left.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7" y="14.25">
+    <polygon points="0,0 -0.25,10.25 49.5,12.25 49,-0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="41">
+  <image width="56" height="118" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Platform_right.PNG"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.5" y="14.5">
+    <polygon points="-1,-0.25 49.25,-0.25 47.5,11.75 -0.5,12"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <image width="142" height="198" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Sky_1.PNG"/>
+ </tile>
+ <tile id="43">
+  <image width="142" height="198" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Sky_2.PNG"/>
+ </tile>
+ <tile id="44">
+  <image width="80" height="80" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Textbox.PNG"/>
+ </tile>
+ <tile id="45">
+  <image width="80" height="80" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Textbox_multianswers.PNG"/>
+ </tile>
+ <tile id="46">
+  <image width="50" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Treasure_box.PNG"/>
+ </tile>
+ <tile id="47">
+  <image width="50" height="50" source="Tiles/die überdrüber coolen Tiles der Christina Planer/Texture/Texture/Treasure_box_open.PNG"/>
+ </tile>
+</tileset>
