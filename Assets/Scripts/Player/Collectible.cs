8f7037a571329f5a51c57dbf146f2d3cca2fdd32
Andreas Kitzberger <kitzberger_a1@outlook.at>
2020-06-18 15:38:01 +0200

    Collection Update, Ammo Update

--- Assets/Scripts/Player/Collectible.cs
+++ Assets/Scripts/Player/Collectible.cs
@@ -1,22 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Collectible : MonoBehaviour
 {
     public int score;
+    public Text text;
+    public GameObject catnip;
+    public BoxCollider2D colider;
+    bool differentCol;
 
     void Start() {
 
         score = 0;
+        differentCol = true;
     }
 
-    void OnTriggerEnter(Collider other) {
-        
-        if (other.tag == "Collectible") {
+    private void Update()
+    {
+        if(score == 0)
+        {
+            text.gameObject.SetActive(false);
+            catnip.gameObject.SetActive(false);            
+        }
+        if(score >= 1)
+        {
+            catnip.SetActive(true);
+            text.gameObject.SetActive(true);
+            text.text = ": " + score;
+        }
+        Debug.Log(score);
+    }
 
+    void OnTriggerEnter2D(Collider2D other) {
+        
+        if (other.CompareTag("Collectible") && differentCol) {
+            Destroy(other);
             score++;
-            Destroy(other.gameObject);
+            Debug.Log(score);
+            differentCol = false;
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Collectible"))
+        {
+            differentCol = true;
         }
     }
 }
