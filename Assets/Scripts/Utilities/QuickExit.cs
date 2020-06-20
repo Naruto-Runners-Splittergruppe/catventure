682a13634dcf4f8963f35fe7936bc8f7b2ea1364
bertibertig <sascha.bertleff@gmail.com>
2020-06-20 15:52:50 +0200

    Stuff for the Development Bild

---
+++ Assets/Scripts/Utilities/QuickExit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickExit : MonoBehaviour
+{
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel") && Debug.isDebugBuild)
+        {
+            Application.Quit();
+        }
+    }
+}
